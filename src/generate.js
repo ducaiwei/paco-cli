d29625c7889102a9cac56387f6e3f1ed143d5f39
DUCAIWEI302 <DUCAIWEI302@pingan.com.cn>
2018-04-24 20:27:25 +0800

    ''

--- src/generate.js
+++ src/generate.js
@@ -19,6 +19,34 @@ Handlebars.registerHelper('unless_eq', function (a, b, opts) {
     return a === b ? opts.inverse(this) : opts.fn(this)
 })
 
+/**
+ *generate a template given a `src` and `dest`
+ *@{String} name
+ * @{String} src
+ * @{String} dest
+ * @{Function} done
+ */
 module.exports = function generate (name, src, dest, done) {
-    
+    const opts = getOptions(name, src)
+    const metalsmith = Metalsmith(path.join(src, 'template'))
+    const data = Object.assign(metalsmith.metadata(), {
+        destDirName: name,
+        inPlace: dest ===  process.cwd(),
+        noEscape: true
+    })
+    opts.helpers && Object.keys(opts.helpers).map(key => {
+        Handlebars.registerHelper(key, opts.helpers[key])
+    })
+    const helpers = { chalk, logger }
+    if (opts.metalsmith && typeof opts.metalsmith.before  === 'function') {
+        opts.metalsmith.before(metalsmith, opts, before)
+    }
+    metalsmith.use(askQuestion(opts.prompts))
+                      .use()
+}
+
+function askQuestion (prompts) {
+    return (files, metalsmith, done) => {
+        ask(prompts, metalsmith.metadata(), done)
+    }
 }
